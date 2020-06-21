99108afbfe532748c2abe8283015886895553719
river-spring <rivermspring@gmail.com>
2020-06-21 20:08:07 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f00c32bffb77190781dd7a4e4eeb5d2c0618d7c63fd6ecce8543128dea02ccfd45d8484fb44b56e25e0ddf96589bf32dafd9cafc0bb0291bb151f2a463d15883'
